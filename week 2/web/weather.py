0992d3d28847583400ac396320bafc3c4e48c81b
Anna Maeva <amaeva911@gmail.com>
2019-09-30 00:26:29 +0300

    Обработала замечания, частично. Залила 1 часть трека Веб по 3 неделе

--- week 2/web/weather.py
+++ week 2/web/weather.py
@@ -10,18 +10,21 @@ def weather_by_city(city_name):
         "num_of_days": "1",
         "lang": "ru"
     }
-    result = requests.get(weather_url, params = params) # возвращает строку, которую необходимо преобразовать
-    weather = result.json() # получение питоновского словаря
+    try:
+        result = requests.get(weather_url, params = params) # возвращает строку, которую необходимо преобразовать
+        result.raise_for_status() # магия с обработкой ответных кодов от сервера
+        weather = result.json() # получение "питоновского словаря"
 
-    if 'data' in weather:
-       if 'currean_condition' in weather['data']:
-           try:
-               return weather['data']['current_condition'][0]
-           except(IndexError, TypeError, ConnectionError, TimeoutError):
-               return False
+        if 'data' in weather:
+            if 'current_condition' in weather['data']:
+                try:
+                    return weather['data']['current_condition'][0] # получение части json
+                except(IndexError,TypeError):
+                    return False
+    except(requests.RequestException,ValueError):
+        print('Сетевая ошибка')
+        return False
     return False
-
-    #return weather
 
 if __name__ == "__main__":
     print(weather_by_city("Moscow, Russia")) # не получилось запустить из-за ConnectionError и TimeoutError :( пробовала и свой ключ и ключ Михаила из видео...
